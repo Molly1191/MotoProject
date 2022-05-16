fa3a2a1f89ac7d3a4bc7949d7582c0f59075dd4a
Sossetosse18 <tony_dahn@hotmail.com>
2022-05-16 22:46:09 +0200

    fruit added

---
+++ Assets/Scripts/FallingObjectController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FallingObjectController : MonoBehaviour
+{
+
+    protected float fallSpeed = 1f;
+    protected float yAxisValue = -1f;
+    protected bool isFalling;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    protected void Fall() 
+    {
+        transform.Translate(new Vector3(0, yAxisValue, 0) * fallSpeed * Time.deltaTime);
+    }
+
+    protected virtual void HitGround() 
+    { 
+        isFalling = false;
+    }
+}
